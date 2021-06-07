bb5b7f8162defa7fa0d0989d828223d63879aae8
Kazuki Otomo <cist.b219.k.otomo@gmail.com>
2021-06-07 14:02:35 +0900

    項目を削除できる機能を追加

--- js/main.js
+++ js/main.js
@@ -7,13 +7,18 @@
     var vm = new Vue({
         el: '#app',
         data: {
-            newItem : '',
+            newItem: '',
             todos: []
         },
         methods: {
             addItem: function () {
                 this.todos.push(this.newItem);
                 this.newItem = ""
+            },
+            deleteItem: function (index) {
+                if (confirm("本当に削除してもよろしいですか？")) {
+                    this.todos.splice(index, 1)
+                }
             }
         }
     })
