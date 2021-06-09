04c2787fd1ca86f6ad67f0dc47e7dd9d2c41c3ff
Kazuki Otomo <cist.b219.k.otomo@gmail.com>
2021-06-09 12:31:14 +0900

    完了したタスクをまとめて削除する機能を追加

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    const vm = new Vue({
+    var vm = new Vue({
         el: '#app',
         data: {
             newItem: '',
@@ -9,7 +9,7 @@
         },
         methods: {
             addItem: function () {
-                const item = {
+                var item = {
                     title: this.newItem,
                     isDone: false
                 };
@@ -17,17 +17,22 @@
                 this.newItem = '';
             },
             deleteItem: function (index) {
-                if (confirm('are you sure?')) {
+                if (confirm('本当に削除しますか?')) {
                     this.todos.splice(index, 1);
                 }
+            },
+            purge: function () {
+                if (!confirm('終了したタスクをまとめて削除しますか?')) {
+                    return;
+                }
+                this.todos = this.remaining;
             }
         },
         computed: {
             remaining: function () {
-                let items = this.todos.filter(function (todo) {
+                return this.todos.filter(function (todo) {
                     return !todo.isDone;
                 });
-                return items.length;
             }
         }
     });
